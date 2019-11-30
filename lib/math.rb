c784d0fe9487a6ddf50c119d35c76492ea8cbde7
wise-integer-6834 <ghaxhaj@fordham.edu>
2019-11-30 23:13:39 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,20 +1,21 @@
 def addition(num1, num2)
-  sum=num!+num2
+  sum=num1+num2
   puts sum
-
 end
 
 def subtraction(num1, num2)
-  
-
+  minus=num1-num2
+  puts minus
 end
 
 def division(num1, num2)
-
+  quotient=num1/num2
+  puts quotient
 end
 
 def multiplication(num1, num2)
-
+  product=num1*num2
+  puts product
 end
 
 def modulo(num1, num2)
